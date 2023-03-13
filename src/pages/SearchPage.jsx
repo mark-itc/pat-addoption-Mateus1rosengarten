2929c388ea7a02c3c458998d5dad9b76d1a9f766
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-03-13 16:07:55 +0200

    Search by Type Feature
    Adding Search by type ( cat / dog ) with checkbox!

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -3,63 +3,101 @@ import SearchComponent from "../ComponentsSearch/SearchBar";
 import { useContext } from "react";
 import { petContext } from "../Context/PetContext";
 import { useNavigate } from "react-router";
-
-
-
+import axios from "axios";
+import { useEffect } from "react";
 
 function SearchPage() {
   const [searchList, setSearchList] = useState(false);
-  const {petInfo, setPetInfo} = useContext(petContext);
-  const [searchParam, setSearchParam] = useState("");
+  const { petInfo, setPetInfo } = useContext(petContext);
+  const { queryType, setQueryType } = useContext(petContext);
+  const { stateDogType, setStateDogType } = useContext(petContext);
+  const { stateCatType, setStateCatType } = useContext(petContext);
+
   const navigate = useNavigate();
 
   const handleSearch = () => {
-    console.log('type search');
-   
+    if (queryType == "Dog") {
+      console.log("Dog", queryType);
+      console.log('dogArray',stateDogType)
+      setStateCatType("");
+      setSearchList(true);
+    }
+    if (queryType == "Cat") {
+      console.log("Cat", queryType);
+      console.log('catArray',stateCatType)
+      setStateDogType("");
+      setSearchList(true);
+    }
+
   };
 
-
-  setTimeout(() => {
-    setSearchList(true)
-   
-  },1000)
-  
- 
 
 
   return (
     <>
       <SearchComponent
-        inputValue={(e) => setSearchParam(e.target.value)}
+        inputCatValue={(e) => setQueryType('Cat')}
+        inputDogValue = {(e) => setQueryType('Dog')}
         handlingBasicSearch={handleSearch}
         setPetInfo={setPetInfo}
       />
 
-   
-      
-       <div className="result-list"> 
-        { searchList && petInfo.map((item) => {
+    
 
-        return    <div className="cardPet">
-        <img src="" alt="" />
-        <div className="containerPet">
-          <h4 className="h4-card">{item.petInfo.name.toUpperCase()}</h4>
-          <p>{item.petInfo.status}</p>
+      <div className="result-list">
+      {queryType === 'Cat' && <h1 className="all-pet-text">All Cats</h1>}
+      {queryType === 'Dog' && <h1 className="all-pet-text">All Dogs</h1>}
+        { queryType === 'Dog' && 
+          stateDogType.map((item) => {
+            return (
+              <> 
+              
+              <div className="cardPet">
+                <img src="" alt="" />
+                <div className="containerPet">
+                  <h4 className="h4-card">{item.petInfo.name.toUpperCase()}</h4>
+                  <p>{item.petInfo.status}</p>
 
-          <button onClick={() => {
-            navigate(`/pet/${item._id}`)
-          }} className="seemore-button">See More</button>
-
-        </div>
-       </div> 
-       
-  
-           
-
-        }) }
-        </div>
+                  <button
+                    onClick={() => {
+                      navigate(`/pet/${item._id}`);
+                    }}
+                    className="seemore-button"
+                  >
+                    See More
+                  </button>
+                </div>
+              </div>
+              </>
+            );
+          })
         
-      
+        }
+
+        { queryType === 'Cat' && (
+          stateCatType.map((item) => {
+            return (
+              <div className="cardPet">
+                <img src="" alt="" />
+                <div className="containerPet">
+                  <h4 className="h4-card">{item.petInfo.name.toUpperCase()}</h4>
+                  <p>{item.petInfo.status}</p>
+
+                  <button
+                    onClick={() => {
+                      navigate(`/pet/${item._id}`);
+                    }}
+                    className="seemore-button"
+                  >
+                    See More
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        ) 
+        }
+      </div>
     </>
   );
 }
