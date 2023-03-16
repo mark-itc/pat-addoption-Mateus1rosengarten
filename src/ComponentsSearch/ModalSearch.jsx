c9f7bca172dcd398f9bbf81678ab118383ba7a5a
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-03-16 02:10:07 +0200

    Fixing delay
    fixing delay of showing return to results based in parameters

--- src/ComponentsSearch/ModalSearch.jsx
+++ src/ComponentsSearch/ModalSearch.jsx
@@ -24,7 +24,7 @@ function ModalSearch({ toggleModal }) {
       setQueryType("Advanced");
       setSearchList(true);
       setModal(!modal)
-    }, 2000);
+    }, 500);
 
     let parameters = {
       name: nameRef.current.value,
