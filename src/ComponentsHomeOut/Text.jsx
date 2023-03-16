9948668d9d1c0e12ef1fd08ad46639dd5b8e7f9e
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-03-17 01:31:52 +0200

    Add files via upload

--- src/ComponentsHomeOut/Text.jsx
+++ src/ComponentsHomeOut/Text.jsx
@@ -4,7 +4,7 @@ function TextWelcoming () {
 
     return( <> 
         <h1 className="h1-style">Welcome to the Pet Adoption Website</h1>
-        <h3 className="h3-style">Our mission is to help people to adopt dogs and cats</h3>
+        <h3 className="h3-style">Our mission is to help people and pets to find a new friend</h3>
         </>
     )
 }
