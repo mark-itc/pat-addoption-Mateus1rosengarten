af3fd6cb3b9137a740e71203185b3f9020a15ff8
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-03-17 01:35:32 +0200

    Changed text

--- src/pages/HomeOut.jsx
+++ src/pages/HomeOut.jsx
@@ -14,7 +14,7 @@ function HomeLogOut() {
       <img
         className="picture-homepage"
         src="https://images.squarespace-cdn.com/content/v1/5c222faff2e6b174633b3c87/1602271575891-SJSK411HS9S2BXR8NDIS/adopt-dog-cat-kitten-puppy-in-Atlanta-Midtown-local?format=2500w"
-        style={{ width: "1500px" }}
+        style={{ width: "100vw" }}
         alt=""
       />
     </>
