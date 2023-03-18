9de7f9658f58d971443d86aeb644646303986a5a
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-03-19 00:09:34 +0200

    Solved error with state containing the user info

--- src/pages/adminPages/UserPage.jsx
+++ src/pages/adminPages/UserPage.jsx
@@ -4,32 +4,32 @@ import axios from "axios";
 import UserCard from "../../ComponentsAdmPages/UserCard";
 
 function UserPage() {
-  const [fullInfoPet, setFullInfoPet] = useState({});
+  const [fullInfoUser, setFullInfoUser] = useState({});
 
   const { id } = useParams();
 
   useEffect(() => {
     if (id) {
-      console.log("id", id);
+      
       axios.get(`http://localhost:3000/user/${id}`).then((res) => {
         console.log("mypey", res.data);
         const userFullInfo = res.data;
         console.log("userinfo", userFullInfo);
-        setFullInfoPet(userFullInfo);
+        setFullInfoUser(userFullInfo);
       });
     }
   }, []);
   return (
     <UserCard
-      name={fullInfoPet.name}
-      lastname={fullInfoPet.lastName}
-      number={fullInfoPet.number}
-      email={fullInfoPet.email}
-      createdAt={fullInfoPet.created_at}
-      role={fullInfoPet.role}
-      adopted={fullInfoPet.adopted}
-      fostered={fullInfoPet.fostered}
-      saved={fullInfoPet.saved}
+      name={fullInfoUser.name}
+      lastname={fullInfoUser.lastName}
+      number={fullInfoUser.number}
+      email={fullInfoUser.email}
+      createdAt={fullInfoUser.created_at}
+      role={fullInfoUser.role}
+      adopted={fullInfoUser.adopted}
+      fostered={fullInfoUser.fostered}
+      saved={fullInfoUser.saved}
     />
   );
 }
